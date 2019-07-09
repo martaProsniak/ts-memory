187236ce6fac160d01142e7526a217ef60655b29
martaprosniak <martaprosniak@gmail.com>
2019-07-09 19:15:06 +0200

    Styles ang html applied

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { MemoryComponent } from './memory/memory.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MemoryComponent
   ],
   imports: [
     BrowserModule
