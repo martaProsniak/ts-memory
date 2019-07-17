11b5720db33f222110ac06f03d844bb46f8c20d1
martaprosniak <martaprosniak@gmail.com>
2019-07-17 14:41:18 +0200

    code reformatting

--- src/app/memory/memory.component.ts
+++ src/app/memory/memory.component.ts
@@ -11,8 +11,7 @@ export class MemoryComponent implements OnInit {
 
   content: Memory
   cards: string[]
-  gameState: GameState
-  bestScore: number
+  public static gameState: GameState
   static bestScore: number = 0;
 
   constructor() {
@@ -21,16 +20,16 @@ export class MemoryComponent implements OnInit {
   }
 
   ngOnInit() {
-    this.draw()
-    this.gameState = new GameState()
-    this.startNewGame(this.shuffleCards(this.cards), this.gameState)
+    this.drawBoard()
+    MemoryComponent.gameState = new GameState()
+    this.startNewGame(this.shuffleCards(this.cards))
   }
 
   start() {
     this.ngOnInit()
   }
 
-  draw() {
+  drawBoard() {
     let board = document.getElementById('board')
     // clear board before starting a new game
     board.innerHTML = ''
@@ -40,22 +39,11 @@ export class MemoryComponent implements OnInit {
       cardBox.classList.add('card')
       cardBox.setAttribute('id', 'c' + i)
 
-      cardBox.style.width = '125px'
-      cardBox.style.height = '125px'
-      cardBox.style.display = 'inline-block'
-      cardBox.style.margin = '7.5px'
-      cardBox.style.backgroundImage = 'url("../../assets/img/card.png")'
-      cardBox.style.border = '4px solid #152e55'
-      cardBox.style.borderRadius = '4px'
-      cardBox.style.cursor = 'pointer'
-      cardBox.style.filter = 'brightness(80%)'
-      cardBox.style.transition = 'all .3s ease-in-out'
-      cardBox.style.boxShadow = '1px 1px 1px 0px #021533';
-
-
+      this.styleCards(cardBox)
 
       board.appendChild(cardBox)
     }
+
     let scoreBox = document.createElement('div')
     scoreBox.classList.add('score')
     scoreBox.setAttribute('id', 'score')
@@ -63,11 +51,11 @@ export class MemoryComponent implements OnInit {
     board.appendChild(scoreBox)
   }
 
-  startNewGame(cards: string[], gameState: GameState) {
+  startNewGame(cards: string[]) {
     let revealCard = this.revealCard
     cards.forEach((card, index, Array) => {
       document.getElementById('c' + index).addEventListener(
-        'click', function () { revealCard(index, Array, gameState); }
+        'click', function () { revealCard(index, Array); }
       )
     });
   }
@@ -95,48 +83,47 @@ export class MemoryComponent implements OnInit {
     return deck;
   }
 
-  revealCard(i: number, array: string[], gameState: GameState) {
+  revealCard(i: number, array: string[]) {
     let cards = array
 
     const card = document.getElementById('c' + i)
     let cardStyle = getComputedStyle(card);
     let opacityValue = parseInt(cardStyle['opacity'])
 
-    if (opacityValue != 0 && !(gameState.lock)) {
-      gameState.lock = true
+    if (opacityValue != 0 && !(MemoryComponent.gameState.lock)) {
+      MemoryComponent.gameState.lock = true
       let image = 'url(' + cards[i] + ')'
 
       card.style.backgroundImage = image;
-      card.classList.add('cardA')
-      card.classList.remove('card')
+     
 
-      if (gameState.oneVisible == false) {
+      if (MemoryComponent.gameState.oneVisible == false) {
         // first card
-        gameState.visibleNr = i
-        gameState.oneVisible = true
-        gameState.lock = false;
+        MemoryComponent.gameState.visibleNr = i
+        MemoryComponent.gameState.oneVisible = true
+        MemoryComponent.gameState.lock = false;
       }
       else {
         // check if not the same as the visible one
-        if (gameState.visibleNr == i) {
-          gameState.oneVisible = true;
-          gameState.lock = false;
+        if (MemoryComponent.gameState.visibleNr == i) {
+          MemoryComponent.gameState.oneVisible = true;
+          MemoryComponent.gameState.lock = false;
           return;
         }
-        if (cards[gameState.visibleNr] == cards[i]) {
+        if (cards[MemoryComponent.gameState.visibleNr] == cards[i]) {
           // pair
           setTimeout(function () {
-            hide2Cards(i, gameState.visibleNr)
+            hide2Cards(i, MemoryComponent.gameState.visibleNr)
           }, 750);
         }
         else {
           // fail
           setTimeout(function () {
-            restore2Cards(i, gameState.visibleNr)
+            restore2Cards(i, MemoryComponent.gameState.visibleNr)
           }, 1000);
         }
-        gameState.turnCounter++
-        document.getElementById('score').innerHTML = 'Turn counter: ' + gameState.turnCounter
+        MemoryComponent.gameState.turnCounter++
+        document.getElementById('score').innerHTML = 'Turn counter: ' + MemoryComponent.gameState.turnCounter
       }
 
     }
@@ -145,12 +132,12 @@ export class MemoryComponent implements OnInit {
       document.getElementById('c' + first).style.opacity = '0'
       document.getElementById('c' + second).style.opacity = '0'
 
-      gameState.pairsLeft--;
-      if (gameState.pairsLeft == 0) {
-        MemoryComponent.createWinAlert(gameState)
+      MemoryComponent.gameState.pairsLeft--;
+      if (MemoryComponent.gameState.pairsLeft == 0) {
+        MemoryComponent.endGame()
       }
-      gameState.oneVisible = false
-      gameState.lock = false
+      MemoryComponent.gameState.oneVisible = false
+      MemoryComponent.gameState.lock = false
     }
 
     function restore2Cards(first: number, second: number) {
@@ -163,36 +150,61 @@ export class MemoryComponent implements OnInit {
         card.classList.remove('cardA')
       });
 
-      gameState.oneVisible = false
-      gameState.lock = false;
+      MemoryComponent.gameState.oneVisible = false
+      MemoryComponent.gameState.lock = false;
     }
 
   }
 
-  static createWinAlert(gameState: GameState) {
-    console.log(MemoryComponent.bestScore)
-    if (MemoryComponent.bestScore === 0 || MemoryComponent.bestScore > gameState.turnCounter) {
-      MemoryComponent.bestScore = gameState.turnCounter
-      console.log(MemoryComponent.bestScore)
+  static endGame() {
+    if (this.bestScore === 0 || this.bestScore > this.gameState.turnCounter) {
+      this.bestScore = this.gameState.turnCounter
+      console.log(this.bestScore)
     }
-
+    MemoryComponent.drawWinAlert();
+  }
+  
+  static drawWinAlert(){
     let board = document.getElementById('board')
     board.innerHTML = ''
     let winAlert = document.createElement('div')
-    winAlert.style.width = '60%'
+    winAlert.style.width = '80%'
     winAlert.style.height = '500px'
     winAlert.style.position = 'relative'
     winAlert.style.marginLeft = 'auto'
     winAlert.style.marginRight = 'auto'
-
+  
     let winAlertWrapper = document.createElement('div')
     winAlertWrapper.style.position = 'absolute';
     winAlertWrapper.style.top = '50%'
     winAlertWrapper.style.left = '50%'
     winAlertWrapper.style.transform = 'translate(-50%, -50%)'
-
-    winAlertWrapper.innerHTML = 'You win!<br>Done in ' + gameState.turnCounter + ' turns'
+    winAlertWrapper.style.width = '100%'
+    winAlertWrapper.style.backgroundImage = 'radial-gradient(circle, #100c21, #13162d, #161d3a, #172547, #152e55, #152e57, #152e5a, #162e5c, #1b2552, #1f1b48, #20113e, #210633)'
+    winAlertWrapper.style.boxShadow = '1px 2px 1px 0px #021533'
+    winAlertWrapper.style.webkitBoxShadow = '1px 2px 1px 0px #021533'
+    
+    let winAlertText = document.createElement('h3')
+    winAlertText.innerHTML = 'You win!<br>Done in ' + MemoryComponent.gameState.turnCounter + ' turns<br>Best score so far: ' + this.bestScore
+    winAlertText.style.width = '100%'
+    winAlertText.style.fontSize = '26px'
+    winAlertText.style.color = '#d6ecf1'
     board.appendChild(winAlert)
     winAlert.appendChild(winAlertWrapper)
+    winAlertWrapper.appendChild(winAlertText)
+  }
+
+  styleCards(cardBox: any){
+    cardBox.style.width = '125px'
+    cardBox.style.height = '125px'
+    cardBox.style.display = 'inline-block'
+    cardBox.style.margin = '7.5px'
+    cardBox.style.backgroundImage = 'url("../../assets/img/card.png")'
+    cardBox.style.border = '4px solid #152e55'
+    cardBox.style.borderRadius = '4px'
+    cardBox.style.cursor = 'pointer'
+    cardBox.style.filter = 'brightness(90%)'
+    cardBox.style.transition = 'all .3s ease-in-out'
+    cardBox.style.boxShadow = '1px 1px 1px 0px #021533';
   }
 }
