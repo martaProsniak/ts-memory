5278f6f2d673899cbddaf44784146d84b3d6b89f
martaprosniak <martaprosniak@gmail.com>
2019-08-09 21:47:27 +0200

    board is moving with function

--- src/app/landing/landing.component.ts
+++ src/app/landing/landing.component.ts
@@ -7,24 +7,68 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LandingComponent implements OnInit {
 
+  textsToDisplay: string[] =
+    ["A long time ago in a galaxy far far away...", "A young, brave adventurer started his cosmic journey...",
+      "Unfortunately...", "...he lost his way!", "Now he must solve the puzzle and match all cards to turn on the autopilot...",
+      "...but his memory has always been a little poor...", "...and everything shakes all the time!",
+      "Would you help him?"]
+  tickDuration: number = 10
 
   constructor() { }
 
   ngOnInit() {
-   this.moveBoard();
+    this.prepareBoard();
+    setInterval(this.changeBoardPosition , this.tickDuration)
+    console.log(this.textsToDisplay);
   }
 
-  moveBoard(){
+  changeBoardPosition(){
+    const board = document.getElementById('board')
+    const speed = 1000
+    const tickDuration = 10
+    const time = tickDuration / 1000
+    const acceleration = 10
+    console.log(time)
+    let boardCurrentPosition = board.offsetTop;
 
-    setTimeout(function(){
-      let boardStart = document.querySelector("div.board-start");
+    if (boardCurrentPosition <= 150){
+      board.style.top = boardCurrentPosition + 'px'
+      return;
+    }
+
+    let boardNewPosition = boardCurrentPosition - speed * time + ((acceleration*time*time) / 2)
+
+    console.log(boardCurrentPosition, boardNewPosition)
+
+    board.style.top = boardNewPosition + 'px'
+  }
+
+
+  prepareBoard() {
+    const board = document.getElementById('board')
+    setBoardStartPosition()
+
+    for (let i = 0; i < this.textsToDisplay.length; i++) {
+      const sentence = this.textsToDisplay[i]
+      displayText(sentence)
+    }
+
+    function displayText(sentence: string){
+        board.className = "board-start"
+        const textBox = document.createElement('h3')
+        textBox.innerHTML = sentence
+        textBox.style.textAlign = 'center'
+        
+        board.appendChild(textBox);
+    }
+
+    function setBoardStartPosition(){
+      board.style.position = 'absolute'
+      board.style.top = '100%'
+      board.style.left = '10%'
+      board.style.transition = 'all 0.1s ease-in'
+    }
      
-      if (boardStart) {
-        boardStart.className = "board-end";
-      } 
-    }, 10)
-
-    
   }
 
 }
