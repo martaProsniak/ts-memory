b307d3a3ba9b438b1feb57af04768c1c032e1a0a
martaprosniak <martaprosniak@gmail.com>
2019-08-11 23:05:19 +0200

    final commit before deploy next version

--- src/app/landing/landing.component.ts
+++ src/app/landing/landing.component.ts
@@ -21,31 +21,31 @@ export class LandingComponent implements OnInit {
 
   ngOnInit() {
     this.prepareIntro();
-    LandingComponent.interval = setInterval(this.changeIntroPosition , this.tickDuration)
+    LandingComponent.interval = setInterval(this.changeIntroPosition, this.tickDuration)
     let container = document.getElementById('event-handler')
-      container.addEventListener('click', function(){
-        LandingComponent.skipIntro()
-      }, {once: true})
+    container.addEventListener('click', function () {
+      LandingComponent.skipIntro()
+    }, { once: true })
   }
 
-  changeIntroPosition(){
+  changeIntroPosition() {
     const intro = document.getElementById('intro')
     const speed = 2000
     const tickDuration = 10
     const time = tickDuration / 1000
     let introCurrentPosition = intro.offsetTop;
 
-    if (introCurrentPosition <= LandingComponent.boardFinalPosition){
+    if (introCurrentPosition <= LandingComponent.boardFinalPosition) {
       intro.style.top = introCurrentPosition + 'px'
       clearInterval(LandingComponent.interval)
       return
     }
 
-    let introNewPosition = introCurrentPosition - speed * time + ((time*time) / 2)
+    let introNewPosition = introCurrentPosition - speed * time + ((time * time) / 2)
     intro.style.top = introNewPosition + 'px'
-    
+
   }
-  
+
 
   prepareIntro() {
     const intro = document.getElementById('intro')
@@ -60,34 +60,29 @@ export class LandingComponent implements OnInit {
     }
     displayImage(alienSrc)
 
-    function displayText(sentence: string){
-        const textBox = document.createElement('h3')
-        const screenSm = 375
-        const screenMd = 768
-        let screenWidth = window.screen.width;
+    function displayText(sentence: string) {
+      const textBox = document.createElement('h3')
+      const screenSm = 375
+      const screenMd = 768
+      let screenWidth = window.screen.width;
 
-        textBox.innerHTML = sentence
-        textBox.style.textAlign = 'center'
-        textBox.style.marginBottom = '10px'
-        if (screenWidth <= screenSm ){
-          textBox.style.fontSize = '14px'
-        } else if (screenWidth <= screenMd){
-          textBox.style.fontSize = '16px'
-        } else {
-          textBox.style.fontSize = '22px'
-        }
-        
-        intro.appendChild(textBox);
+      textBox.innerHTML = sentence
+      textBox.style.textAlign = 'center'
+      textBox.style.marginBottom = '10px'
+      if (screenWidth <= screenSm) {
+        textBox.style.fontSize = '14px'
+      } else if (screenWidth <= screenMd) {
+        textBox.style.fontSize = '16px'
+      } else {
+        textBox.style.fontSize = '22px'
+      }
+
+      intro.appendChild(textBox);
     }
 
-    function displayImage(source: string){
+    function displayImage(source: string) {
       const imageBox = document.createElement('div')
-      const screenMd = 768
-      if(window.screen.width > screenMd){
-        imageBox.style.width = '75px'
-      } else {
-        imageBox.style.width = '50px'
-      }
+      imageBox.style.width = '50px'
       imageBox.style.height = 'auto'
       imageBox.style.marginRight = 'auto'
       imageBox.style.marginLeft = 'auto'
@@ -98,26 +93,26 @@ export class LandingComponent implements OnInit {
       heroImg.setAttribute('src', source)
       heroImg.style.width = '100%'
       heroImg.style.height = '100%'
-      
+
       imageBox.appendChild(heroImg)
       intro.appendChild(imageBox)
     }
 
-    function setIntroStartPosition(){
-       intro.style.position = 'absolute'
-       intro.style.top = '100%'
+    function setIntroStartPosition() {
+      intro.style.position = 'absolute'
+      intro.style.top = '100%'
       intro.style.left = '50%'
       intro.style.transform = 'translateX(-50%)'
       intro.style.transition = 'all 0.1s ease-in'
     }
   }
 
-  play(){
+  play() {
     clearInterval(LandingComponent.interval)
     this.router.navigateByUrl('/game')
   }
 
-  static skipIntro(){
+  static skipIntro() {
     const intro = document.getElementById('intro')
     intro.style.transition = 'all 0.5s ease-in'
     intro.style.top = LandingComponent.boardFinalPosition + 'px'
