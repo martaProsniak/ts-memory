085343c2bd4d9e45c535114b6285a9b290abb44b
martaprosniak <martaprosniak@gmail.com>
2020-07-19 00:18:45 +0200

    Purecss added, focus outline removed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: [
+    "./app.component.css",
+    "../../node_modules/purecss/build/pure-min.css",
+  ],
 })
 export class AppComponent {
-  title = 'memory';
+  title = "memory";
 }
